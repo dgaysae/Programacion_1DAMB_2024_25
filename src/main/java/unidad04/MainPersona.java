81bdeb014a81edc0f3e5bafb3e72a649a55a4d70
dgaysae <dgaysae524@g.educaand.es>
2024-11-14 09:44:41 +0100

    Unidad 04
    Sobreescribimos métodos clone(), equals() y toString() heredados de Objetc.

---
+++ src/main/java/unidad04/MainPersona.java
@@ -0,0 +1,39 @@
+package unidad04;
+
+/**
+ * 
+ * @author diego
+ */
+public class MainPersona {
+    
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Pepe", "López Sánchez", 45, 1.92);
+        System.out.println(p1);
+        
+        System.out.println("---------- clon superficial ----------");
+        Persona clonSuperficial = p1;
+        System.out.println(clonSuperficial);
+        System.out.println("El clon superficial y el objeto son iguales? "
+                + (p1.equals(clonSuperficial)));
+        
+        clonSuperficial.setNombre("José");
+        System.out.println(p1);
+        
+        System.out.println("---------- clon en profundidad ----------");
+        Persona clonProfundidad = p1.clone();
+        System.out.println(clonProfundidad);
+        System.out.println("El clon en profundidad y el objeto son iguales? "
+                + (p1.equals(clonProfundidad)));
+
+        p1.setNombre("Anselmo");
+        System.out.println("El clon en profundidad y el objeto son iguales? "
+                + (p1.equals(clonProfundidad)));
+        
+        
+        clonProfundidad.setEdad(89);
+        System.out.println(clonProfundidad);
+        System.out.println(p1);
+        
+    }
+    
+}
---
+++ src/main/java/unidad04/Persona.java
@@ -0,0 +1,132 @@
+package unidad04;
+
+/**
+ *
+ * @author diego
+ */
+public class Persona {
+    
+    // Atributos de la clase
+    protected static final int EDAD_MINIMA = 11;
+    public static final double ESTATURA_MEDIA = 1.50;
+    
+    private static int contadorPersonas = 0;
+    private String nombre = "";
+    private String apellidos = "";
+    private int edad = 0;
+    private double altura = 0;
+    
+    // Constructores
+    public Persona(String nombre,
+            String apellidos,
+            int edad,
+            double altura) {
+        
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        setEdad(edad);
+        setAltura(altura);
+        
+        contadorPersonas++;
+    }
+    
+    public Persona(String nombre,
+            String apellidos,
+            double altura) {
+
+        this(nombre, apellidos, EDAD_MINIMA, altura);
+/*
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        edad = EDAD_MINIMA;
+        setAltura(altura);
+*/
+    }
+    
+    public Persona(String nombre, String apellidos) {
+        this(nombre, apellidos, ESTATURA_MEDIA);
+    }
+    
+    // Getters / Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    private String filtrarCaracteres(String texto) {
+        final String NO_PERMITIDOS = "0123456789";
+        String cadenaFiltrada = "";
+        
+       // texto.replace(NO_PERMITIDOS.charAt(0), "");
+        
+        return cadenaFiltrada;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setEdad(int edad) {
+        if (edad < EDAD_MINIMA){
+            this.edad = EDAD_MINIMA;
+        }            
+        else{
+            this.edad = edad;
+        }
+        
+        //La siguientes es equivalente, usando el operador ternario ? :
+        //this.edad = (edad < 0) ? 0 : edad;
+        
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = (altura < 0) ? ESTATURA_MEDIA : altura;
+    }
+    
+    public double getAltura() {
+        return altura;
+    }
+    
+    public int getContadorPersonas() {
+        return contadorPersonas;
+    }
+
+    // Métodos
+    /**
+     * 
+     */
+    static public void saludar() {
+        System.out.println("Hola!!");
+    }
+    
+    @Override
+    public String toString() {
+        return "Persona{"
+                + "Nombre: " + nombre
+                + ", Apellidos: " + apellidos
+                + ", Edad: " + edad
+                + ", Altura: " + altura
+                + "}";
+    }
+    
+    @Override
+    public Persona clone() {
+        return new Persona(nombre, apellidos, edad, altura);
+    }
+    
+    public boolean equals(Persona p) {
+        return this.nombre.equals(p.nombre)
+                && this.apellidos.equals(p.apellidos);
+    }
+}
