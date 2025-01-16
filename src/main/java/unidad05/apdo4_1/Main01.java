3bce58a98e7d91af820edc3b5eab7bdcdb69c1e2
Diego Gay Sáez <dgaysae524@g.educaand.es>
2025-01-16 21:23:50 +0100

    Unidad 05 - ArrayList y cómo influye el método equals para borrar objetos.

--- src/main/java/unidad05/apdo4_1/Main01.java
+++ src/main/java/unidad05/apdo4_1/Main01.java
@@ -30,6 +30,7 @@ public class Main01 {
         System.out.printf("Nº elementos de la lista %d: %n", listaNombres.size());
 
         // Obtener elementos de la lista:
+        System.out.println("Obtener elementos de la lista: ------");
         String nombre = listaNombres.get(indice);
         System.out.printf("Nombre en posicion %d: %s%n", indice, nombre);
         System.out.printf("Nombre en posicion %d: %s%n", indice, listaNombres.get(1));
@@ -47,12 +48,39 @@ public class Main01 {
         listaNombres.set(0, "Anselmo");
         System.out.println("Lista de nombres: " + listaNombres);
 
+        // Eliminar un objeto de la lista:
+        System.out.println("Eliminar un nombre de la lista: ------");
+        System.out.println("Lista de nombres: " + listaNombres);
+
+        // Indicando el objeto a borrar:
+        listaNombres.remove("José");
+        System.out.println("Lista de nombres: " + listaNombres);
+
+        // Indicando la posición a borrar:
+        listaNombres.remove(0);
+        System.out.println("Lista de nombres: " + listaNombres);
+
         System.out.println("----------------------------------------");
         ArrayList<Integer> listaNotas = new ArrayList();
         listaNotas.add(9);
         listaNotas.add(4);
         listaNotas.add(9);
         System.out.println("Lista de notas: " + listaNotas);
+
+        // Obtener tamaño de la lista (nº de elementos):
+        System.out.printf("Nº elementos de la lista %d: %n", listaNotas.size());
+
+        // Obtener elementos de la lista:
+        System.out.println("Obtener elementos de la lista: ------");
+        indice = 1;
+        int nota = listaNotas.get(indice);
+        System.out.printf("Elemento en posicion %d: %d%n", indice, nota);
+
+        // Cambiar valor de un elemento de la lista:
+        System.out.println("Cambiar uno de los elementos: ------");
+        System.out.println("Lista de elementos: " + listaNotas);
+        listaNotas.set(0, 5);
+        System.out.println("Lista de elementos: " + listaNotas);
 
         System.out.println();
 
@@ -63,5 +91,36 @@ public class Main01 {
         listaNeumaticos.add(new Neumatico(40, 40,40000));
 
         System.out.println("Lista de neumáticos: " + listaNeumaticos);
+
+        // Obtener tamaño de la lista (nº de elementos):
+        System.out.printf("Nº elementos de la lista %d: %n", listaNeumaticos.size());
+
+        // Obtener elementos de la lista:
+        System.out.println("Obtener elementos de la lista: ------");
+        indice = 1;
+        System.out.println("Elemento en posicion "
+                + indice + ": "
+                + listaNeumaticos.get(indice)
+        );
+
+        // Cambiar valor de un elemento de la lista:
+        System.out.println("Cambiar uno de los elementos: ------");
+        System.out.println("Lista de elementos: " + listaNeumaticos);
+        listaNeumaticos.set(0,
+                new Neumatico(5, 5,50000));
+        System.out.println("Lista de elementos: " + listaNeumaticos);
+
+        // Eliminar un objeto de la lista:
+        System.out.println("Eliminar un elemento de la lista: ------");
+        System.out.println("Lista de elementos: " + listaNeumaticos);
+        listaNeumaticos.remove(0);
+        /*
+        Para usar .remove(Object o) la lista compara el objeto introducido con cada uno de los que
+        tiene almacenados. Si alguno es igual al introducido, se borra.
+        Pero ¿cómo sabe la lista que son iguales? Por el método equals del objeto.
+        Como no lo hemos definido, comprueba que el objeto introducido haga referencia
+        a cada objeto que vaya comprobando.
+        */
+        System.out.println("Lista de elementos: " + listaNeumaticos);
     }
 }
