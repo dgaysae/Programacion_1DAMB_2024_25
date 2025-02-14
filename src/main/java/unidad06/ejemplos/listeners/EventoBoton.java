fd9d51fa3b2ea2490d2fe923da8ca8f91a1887ce
dgaysae <dgaysae524@g.educaand.es>
2025-02-14 13:57:09 +0100

    Unidad 06. Ejemplos de listeners.

---
+++ src/main/java/unidad06/ejemplos/listeners/EventoBoton.java
@@ -0,0 +1,17 @@
+package unidad06.ejemplos.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author diego
+ */
+public class EventoBoton implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        System.out.println("Has pulsado el botón");
+    }
+    
+}
---
+++ src/main/java/unidad06/ejemplos/listeners/Main.java
@@ -0,0 +1,13 @@
+package unidad06.ejemplos.listeners;
+
+/**
+ * 
+ * @author diego
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Ventana ventana = new Ventana(null);
+        
+    }
+}
